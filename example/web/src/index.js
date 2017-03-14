e3de80271007b13be75bcfb8b7e2da4a45c75d43
Augustin Borsu <augustin@sagacify.com>
2017-03-14 01:11:44 +0100

    feat(typescript): loader
    Simplify preset to only take care of adding typescript so it can be combined with other presets. (Such as web, react or node).
    Created working examples of use with web or react.

---
+++ example/web/src/index.js
@@ -0,0 +1,5 @@
+const app = document.createElement('main');
+const text = document.createTextNode('Hello world!');
+
+app.appendChild(text);
+document.getElementById('root').appendChild(app);
--- src/index.js
+++ src/index.js
@@ -1,37 +1,30 @@
-
 'use strict';
 
-const web = require('neutrino-preset-web');
-const merge = require('deepmerge');
 const path = require('path');
-const webpack = require('webpack');
 
 const CWD = process.cwd();
 const SRC = path.join(CWD, 'src');
-const TS_LOADER = require.resolve('ts-loader');
-const MODULES = path.join(__dirname, '../node_modules');
+const TS_LOADER = require.resolve('awesome-typescript-loader');
 
 module.exports = neutrino => {
-  web(neutrino); // Start with neutrino-preset-web
-
   const { config } = neutrino; // Maps to basic webpack config options
 
   // Replace index.js with index.ts
   config
     .entry('index')
     .clear()
-    .add(path.join(SRC, 'index.ts'))
-    .end()
+    .add(path.join(SRC, 'index'))
+    .end();
 
   // Typescript compile rules
   config.module
     .rule('compile')
     .test(/\.tsx?$/)
-    .loader('ts-loader', TS_LOADER);
+    .loader('ts-loader', TS_LOADER, {
+      jsx: 'react'
+    });
 
   // Resolve
-  config.resolve.modules.add(MODULES);
   config.resolve.extensions.add('.ts');
-  config.resolveLoader.modules.add(MODULES);
-
-}
+  config.resolve.extensions.add('.tsx');
+};
