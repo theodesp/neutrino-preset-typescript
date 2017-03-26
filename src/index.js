a879c03a4cf6237302693f9cf72ea43df15afd58
Matt Lane <lanemt@gmail.com>
2017-03-26 04:21:17 -0400

    Add copy config to ignore typescript files
    Prevents typescript files from being copied to output dir
    when running 'neutrino build'

--- src/index.js
+++ src/index.js
@@ -5,9 +5,17 @@ const path = require('path');
 const CWD = process.cwd();
 const SRC = path.join(CWD, 'src');
 const TS_LOADER = require.resolve('awesome-typescript-loader');
+const copy = require('neutrino-middleware-copy');
 
 module.exports = neutrino => {
   const { config } = neutrino; // Maps to basic webpack config options
+
+  if (process.env.NODE_ENV !== 'development') {
+    neutrino.use(copy, {
+      patterns: [{ context: neutrino.options.source, from: '**/*' }],
+      options: { ignore: ['*.ts*'] }
+    });
+  }
 
   // Replace index.js with index.ts
   config
